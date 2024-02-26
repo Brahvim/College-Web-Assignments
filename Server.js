6ebe2766b2730b9e1771af0750c1e37be8532ad7
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-26 16:05:23 +0530

    STUFF!

--- Server.js
+++ Server.js
@@ -6,6 +6,22 @@ const SERVER = HTTP.createServer((p_request, p_response) => {
     const requestedUrl = p_request.url;
     console.log("Resource `" + requestedUrl + "` was requested.");
 
+    p_response.sendContent = (p_content, p_type) => {
+        p_response.writeHead(200, { "Content-Type": p_type });
+        p_response.end(p_content);
+    };
+
+    p_response.sendFile = (p_content, p_type) => {
+        p_response.writeHead(200, { "Content-Type": p_type });
+        p_response.end(p_content);
+    };
+
+    // p_response.sendContent = function (p_content, p_type) {
+    //     this.writeHead(200, { "Content-Type": p_type });
+    //     this.end(p_content);
+    // }.bind(p_response); // `bind()` sets `this` to be `p_response` for `sendContent()`.
+    // ...but I'll have to use an arrow function / lambda, which I didn't want to :>
+
     switch (requestedUrl) {
         default: {
             console.log("Received request for unknown page...");
@@ -13,7 +29,7 @@ const SERVER = HTTP.createServer((p_request, p_response) => {
         } break;
 
         case "/": {
-            FILE_SYSTEM.readFile("./Home.html", "utf8", (p_error, p_fileData) => {
+            FILE_SYSTEM.readFile("./Home.html", (p_error, p_fileData) => {
                 if (p_error) {
                     console.log("No such file!");
                     writeFailureResponse(p_response);
@@ -24,10 +40,42 @@ const SERVER = HTTP.createServer((p_request, p_response) => {
                 p_response.end(p_fileData);
             });
         } break;
+
+        case "/MyImage.jpeg": {
+            FILE_SYSTEM.readFile("./MyImage.jpeg", (p_error, p_fileData) => {
+                if (p_error) {
+                    console.log("No such file!");
+                    writeFailureResponse(p_response);
+                }
+
+                console.log("Sending image!");
+
+                p_response.writeHead(200, { "Content-Type": "image/jpeg" });
+                p_response.end(p_fileData);
+            });
+        } break;
+
+        case "/Styling.css": {
+            newFunction(p_response);
+        } break;
     }
 });
 
 SERVER.listen(PORT);
+
+function newFunction(p_response) {
+    FILE_SYSTEM.readFile("./Styling.css", (p_error, p_fileData) => {
+        if (p_error) {
+            console.log("No such file!");
+            writeFailureResponse(p_response);
+        }
+
+        console.log("Sending CSS!");
+
+        p_response.writeHead(200, { "Content-Type": "text/css" });
+        p_response.end(p_fileData);
+    });
+}
 
 function writeFailureResponse(p_response) {
     console.log("Sending status code `500`...");
