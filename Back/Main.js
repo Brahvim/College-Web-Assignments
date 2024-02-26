592be62d65cd83bec202fe6ee5199aadcd1917da
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-26 22:26:46 +0530

    Debugged my way through source paths. Yay!

--- Back/Main.js
+++ Back/Main.js
@@ -1,43 +1,89 @@
 const HTTP = require("http");
+const PATHS = require("path");
 const FILE_SYSTEM = require("fs");
 
 const PORT = 8080;
+// (Omitted the `/` for consistency with the `/` already present in requests.)
+
 const SERVER = HTTP.createServer((p_request, p_response) => {
     if (p_request.url === "/")
         p_request.url = "/Home.html";
 
+    // Surprised that a `../` is not allowed in the browser anyway.
+
     if (p_request.url.endsWith(".html"))
-        p_request.url = "../Front/Pages" + p_request.url;
+        p_request.url = (__dirname + "/../Front/Pages") + p_request.url;
     else if (p_request.url.endsWith(".css"))
-        p_request.url = "../Front/Pages" + p_request.url;
+        p_request.url = (__dirname + "/../Front/Styling") + p_request.url;
     else
-        p_request.url = "../Front/Resources" + p_request.url;
+        p_request.url = (__dirname + "/../Resources") + p_request.url;
+
+    // console.log(__dirname);
+    // console.log(FILE_SYSTEM.existsSync(__dirname));
+    // console.log(__dirname + "../");
+    // console.log(FILE_SYSTEM.existsSync(__dirname + "../"));
+    // console.log(__dirname + "../Resources");
+    // console.log(FILE_SYSTEM.existsSync(__dirname + "../Resources"));
+    // console.log(__dirname + "../Front");
+    // console.log(FILE_SYSTEM.existsSync(__dirname + "../Front"));
+    // console.log(__dirname + "../Front/Pages");
+    // console.log(FILE_SYSTEM.existsSync(__dirname + "../Front/Pages"));
 
     console.log("Resource `" + p_request.url + "` was requested.");
 
+    let contentType;
+
+    switch (p_request.url.substring(p_request.url.lastIndexOf("."))) {
+        case "css":
+            contentType = "text/css";
+            break;
+
+        case "jpeg":
+            contentType = "image/jpeg";
+            break;
+
+        case "html":
+            contentType = "text/html";
+            break;
+
+        default:
+            contentType = "";
+            break;
+    }
+
     // Do these callbacks always get called...?
-    FILE_SYSTEM.readFile(p_request.url, "utf8", (p_error, p_fileData) => {
-        let code, header, data; // Not using an object here!
-        // Old browsers shall have the speed we need!
+    sendFileViaResponse(p_response, p_request.url, contentType);
+});
+
+SERVER.listen(PORT);
+
+function sendFileViaResponse(p_response, p_filename, p_contentType = "text/html") {
+    // Do these callbacks always succeed...?
+    FILE_SYSTEM.readFile(p_filename, (p_error, p_fileData) => {
+        let code, header, data;
 
         if (p_error) {
-            code = 400;
-            data = "Resource not found...";
+            code = 500;
+            data = "Internal Server Error!";
             header = { "Content-Type": "text/plain" };
 
-            console.log("No file like `" + p_request.url + "` exists!");
+            console.log("No file like `" + p_filename + "` exists!");
             console.log("Sending status code `500`...");
+
         } else {
             code = 200;
             data = p_fileData;
             header = { "Content-Type": p_contentType };
 
-            console.log("Sending file `" + p_request.url + "`!");
+            console.log("Sending `" + p_filename + "`!");
         }
 
         p_response.writeHead(code, header);
         p_response.end(data);
     });
-});
+}
 
-SERVER.listen(PORT);
+function sendContentViaResponse(p_response, p_content, p_contentType = "text/plain", p_code = 200) {
+    p_response.writeHead(p_code, { "Content-Type": p_contentType });
+    p_response.end(p_content);
+}
