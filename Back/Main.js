9689089c64ca2ae6f680d12047865e1b006ca431
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-26 21:36:14 +0530

    Working, working, working!
    Debugging, actually. Haha.

--- Back/Main.js
+++ Back/Main.js
@@ -3,51 +3,41 @@ const FILE_SYSTEM = require("fs");
 
 const PORT = 8080;
 const SERVER = HTTP.createServer((p_request, p_response) => {
-    const requestedUrl = p_request.url;
-    console.log("Resource `" + requestedUrl + "` was requested.");
+    if (p_request.url === "/")
+        p_request.url = "/Home.html";
 
-    sendFileViaResponse(p_response);
+    if (p_request.url.endsWith(".html"))
+        p_request.url = "../Front/Pages" + p_request.url;
+    else if (p_request.url.endsWith(".css"))
+        p_request.url = "../Front/Pages" + p_request.url;
+    else
+        p_request.url = "../Front/Resources" + p_request.url;
 
-    // p_response.sendContent = (p_content, p_type) => {
-    //     p_response.writeHead(200, { "Content-Type": p_type });
-    //     p_response.end(p_content);
-    // };
+    console.log("Resource `" + p_request.url + "` was requested.");
 
-    // p_response.sendContent = function (p_content, p_type) {
-    //     this.writeHead(200, { "Content-Type": p_type });
-    //     this.end(p_content);
-    // }.bind(p_response); // `bind()` sets `this` to be `p_response` for `sendContent()`.
-    // ...but I'll have to use an arrow function / lambda, which I didn't want to :>
+    // Do these callbacks always get called...?
+    FILE_SYSTEM.readFile(p_request.url, "utf8", (p_error, p_fileData) => {
+        let code, header, data; // Not using an object here!
+        // Old browsers shall have the speed we need!
 
-    if (requestedUrl === "/")
-        requestedUrl = "/Home.html";
+        if (p_error) {
+            code = 400;
+            data = "Resource not found...";
+            header = { "Content-Type": "text/plain" };
 
+            console.log("No file like `" + p_request.url + "` exists!");
+            console.log("Sending status code `500`...");
+        } else {
+            code = 200;
+            data = p_fileData;
+            header = { "Content-Type": p_contentType };
+
+            console.log("Sending file `" + p_request.url + "`!");
+        }
+
+        p_response.writeHead(code, header);
+        p_response.end(data);
+    });
 });
 
 SERVER.listen(PORT);
-
-// Won't make an entirely new file to import dependencies into JUST for this:
-function sendFileViaResponse(p_response, p_filename, p_contentType = "text/plain") {
-    // Do these callbacks always succeed...?
-    FILE_SYSTEM.readFile(p_filename, (p_error, p_fileData) => {
-        const response = { code: null, header: null, data: null };
-
-        if (p_error) {
-            response.code = 500;
-            response.data = "Internal Server Error!";
-            response.header = { "Content-Type": "text/plain" };
-
-            console.log("No file like `" + p_filename + "` exists!");
-            console.log("Sending status code `500`...");
-
-        } else {
-            response.code = 200;
-            response.data = p_fileData;
-            response.header = { "Content-Type": p_contentType };
-            console.log("Sending `" + p_filename + "`!");
-        }
-
-        p_response.writeHead(response.code, response.header);
-        p_response.end(response.data);
-    });
-}
