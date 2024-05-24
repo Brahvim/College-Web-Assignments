59dd48e0adc367a88c339a4dfbf8d9b99215b61f
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-24 23:31:43 +0530

    Published 🙏

--- Front/JavaScript/Home.js
+++ Front/JavaScript/Home.js
@@ -1,9 +1,36 @@
 
-document.on('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelector('#fade-in table tr') // Hmmm...! So the multi-token parsing isn't just a jQuery feature!...
-        .rows.forEach((row, id) => {
-            setTimeout(() => row.classList.add('fade-in'), id * 30);
-        });
+    const timeTable = document.getElementById('time-table');
+    const tableName = document.getElementById('table-name');
+    const rows = document.querySelectorAll('tr');
+    const delay = 100;
+
+    const tableNameStr = "-lunch Time-table:";
+    const tableNameStrLenMinusOne = tableNameStr.length - 1;
+
+    let lastCharId = 0;
+    timeTable.style.opacity = 0;
+
+    const textAddInterval = setInterval(() => {
+        if (lastCharId >= tableNameStrLenMinusOne)
+            clearInterval(textAddInterval);
+
+        const char = tableNameStr[lastCharId++];
+        tableName.innerHTML += char;
+        document.title += char;
+    }, 90);
+
+    rows.forEach((row, id) =>
+        setTimeout(
+            () => row.classList.add('fade-in'),
+            id * delay
+        )
+    );
+
+    setTimeout(
+        () => timeTable.style.opacity = 1,
+        rows.length * delay * 4
+    );
 
 });
