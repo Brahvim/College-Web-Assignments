6633ab6753dc590e40e89f57288f92069508f00a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-25 00:26:56 +0530

    Donner!

---
+++ Front/JavaScript/Home.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const promptText = document.getElementById("prompt-text");
+    const originalText = promptText.innerText;
+    let uin = ""; // "**U**ser **IN**put" :>
+
+    document.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case "Backspace": {
+                uin = "";
+                e.preventDefault();
+                promptText.innerText = originalText;
+            } break;
+
+            case ' ': {
+                uin += e.key;
+                e.preventDefault();
+
+                // ...because `promptText.innerText += ' '` doesn't work...?
+                promptText.innerText = originalText + uin;
+            } break;
+
+            default: {
+                if (e.key.length === 1) {
+                    uin += e.key.toUpperCase();
+                    promptText.innerText = originalText + uin;
+                }
+            }
+        }
+    });
+
+});
