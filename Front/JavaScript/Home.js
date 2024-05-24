a7fc2885782740d40482e982b615c7b9adcce36f
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-24 21:17:16 +0530

    Lovely colors!

--- Front/JavaScript/Home.js
+++ Front/JavaScript/Home.js
@@ -16,28 +16,89 @@ document.addEventListener("DOMContentLoaded", () => {
     elementsToAddFadeIn = elementsToAddFadeIn
         .filter(e => e.nodeName === "DIV");
 
-    if (elementsToAddFadeIn.length > 0)
-        elementsToAddFadeIn.forEach(e => {
-            e.classList.add("fade-in");
-            console.log(e);
-        });
-
-    // Get all elements that have our `.fade-in` class:
-    const elements = document.querySelectorAll('.fade-in');
+    elementsToAddFadeIn.forEach(e => {
+        e.classList.add("fade-in");
+        console.log(e);
+    });
 
     // Function to check bounds:
     const isElementInViewport = (e) => {
         const rect = e.getBoundingClientRect();
 
-        return rect.left < window.scrollX + window.innerWidth
-            && rect.top < window.scrollY + window.innerHeight;
+        // return (
+        //     rect.left < window.scrollX + window.innerWidth
+        //     && rect.top < window.scrollY + window.innerHeight
+        // );
+
+        return (
+            rect.left < window.innerWidth
+            && rect.top < window.innerHeight
+            && rect.right > 0
+            && rect.bottom > 0
+        );
+
+        // return !(
+        //     // rect.top >= 0 &&
+        //     rect.left >= window.scrollX
+        //     && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        //     // && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        //     && rect.top > window.scrollY
+        //     && rect.bottom > window.scrollY
+        // );
     };
+
+    // const elements = document.querySelectorAll('.fade-in');
 
     // Function to add elements to the `visible` class when needed:
     const addToVisibleClass = () => {
-        for (let e of elements)
+        // Get all elements that have our `.fade-in` class:
+        const makeVisible = (e) => e.classList.add('visible');
+        const makeInvisible = (e) => e.classList.remove('visible');
+        const makeVisibleDelayed = (e) => setTimeout(500, makeVisible, e);
+        const randomRgb = () => `rgb(${Math.random() * 255} ${Math.random() * 255} ${Math.random() * 255})`;
+
+        const allElements
+            = document.querySelectorAll('.fade-in');
+        let insideElements = [];
+        let outsideElements = [];
+
+        for (let e of allElements) {
             if (isElementInViewport(e))
-                e.classList.add('visible');
+                insideElements.push(e);
+            else
+                outsideElements.push(e);
+        }
+
+        for (let e of insideElements) {
+            if (!e.classList.contains("visible")) {
+                makeVisible(e);
+                e.style.cssText =
+                    e.style.cssText + (`color: ${randomRgb()}`);
+            }
+        }
+
+        for (let e of outsideElements)
+            makeInvisible(e);
+
+        // Don't use this! I've made the entire thing above be more vectorizable!...
+        // ...or that it's better at using cache.
+        // This *is* slower, but it works!:
+
+        // for (let e of document.querySelectorAll('.fade-in')) {
+        //     if (isElementInViewport(e)) {
+        //         if (e.classList.contains("visible"))
+        //             return;
+        //
+        //      makeVisible(e);
+        //
+        //         // Thank GOD I don't have to parse this instead of setting and forgetting!:
+        //         let css = e.style.cssText;
+        //         css = css + (`color: ${randomRgb()}`);
+        //         e.style.cssText = css;
+        //     }
+        //     else
+        //         makeInvisible(e);
+        // }
     };
 
     // These listeners do it, too...:
