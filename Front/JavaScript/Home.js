0e2634c436b9685335d7f3ad12b7d2e13ccc20e4
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-25 12:10:12 +0530

    I won't work on this now...

--- Front/JavaScript/Home.js
+++ Front/JavaScript/Home.js
@@ -1,2 +1,42 @@
 
-$(document).ready(() => $(".centered-element").hide().fadeIn('slow'));
+$(document).ready(() => {
+
+    const blinkDelay = 250;
+    const submitButton = $("#submit");
+    const blinkDelayHalf = blinkDelay / 2;
+    const blinkElement = $("#blink-element");
+
+    let nameFilled = false;
+    let emailFilled = false;
+    let passwordFilled = false;
+
+    submitButton.hide();
+    $(".centered-element").hide().fadeIn('slow');
+
+    setInterval(() => {
+        if (!(nameFilled && emailFilled && passwordFilled))
+            return;
+        submitButton.show();
+    }, 10);
+
+    submitButton.click(function (p_event) {
+        console.log(nameFilled, emailFilled, passwordFilled);
+
+        if (!(nameFilled && emailFilled && passwordFilled)) {
+            // console.log("Submit event!");
+            return;
+        }
+
+        // console.log("Ready to go!");
+
+        setInterval(() => {
+            blinkElement.show();
+            setTimeout(() => blinkElement.hide(), blinkDelayHalf);
+        }, blinkDelay);
+    });
+
+    $('#name').on('input', function () { nameFilled = $(this).val().length > 1; });
+    $('#email').on('input', function () { emailFilled = $(this).val().length > 1; });
+    $('#password').on('input', function () { passwordFilled = $(this).val().length > 1; });
+
+});
