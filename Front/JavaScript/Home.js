e48fe81ce4ae439f5a83154250181c2641334e2f
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-25 08:34:51 +0530

    Ugh. Done.

--- Front/JavaScript/Home.js
+++ Front/JavaScript/Home.js
@@ -1,26 +1,2 @@
-$(document).ready(() => { // Callback for when stuff is ready!
 
-    let clickCount = 0;
-    let firstClickTime = 0;
-
-    $("p").click(() => { // Callback fired when ANY paragraph gets clicked.
-        clickCount++;
-
-        switch (clickCount) {
-
-            case 1: {
-                firstClickTime = new Date().getTime();
-            } break;
-
-            case 2: {
-                clickCount = 0;
-                const secondClickTime = new Date().getTime();
-                const elapsedTime = secondClickTime - firstClickTime;
-                $("#timer").text("Time between clicks was: `" + elapsedTime + "` ms.");
-            } break;
-
-        }
-
-    });
-
-});
+$(document).ready(() => $(".centered-element").hide().fadeIn('slow'));
